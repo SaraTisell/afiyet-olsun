c1e917794784aef729e5f981b199e4808936abbe
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-08 10:51:55 +0000

    Add validators to review model

--- reviews/models.py
+++ reviews/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField()
+    rating = models.IntegerField(default=0,
+        validators=[
+            MaxValueValidator(5),
+            MinValueValidator(0)
+        ])
     content = models.TextField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
