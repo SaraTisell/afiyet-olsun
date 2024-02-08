99b121c5e6c0c184ef213c9646f3eca823d69c5c
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-08 10:47:56 +0000

    Create review model & import value validators

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    content = models.TextField(max_length=150)
+    created_at = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
