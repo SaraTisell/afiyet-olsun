aca96b2afc3fac6a6f03b105287217a6d9bcdcd5
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-08 09:22:58 +0000

    Add favicon

--- afiyet_olsun/settings.py
+++ afiyet_olsun/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-saratisell-afiyetolsun-k7uqudbu3j3.ws-eu108.gitpod.io', '.herokuapp.com']
 
@@ -133,7 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
