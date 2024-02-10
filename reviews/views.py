da9a91cf185a62caf5d639ce819cc47a893aa881
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-10 09:31:31 +0000

    Add order_by  & filter to display latest/ publish/approved

--- reviews/views.py
+++ reviews/views.py
@@ -4,5 +4,5 @@ from .models import Review
 
 # Create your views here.
 class PostList(generic.ListView):
-    queryset = Review.objects.all()
-    template_name = "review_list.html"
+    queryset = Review.objects.filter(status=1, approved=True).order_by('created_at')
+#    template_name = "review_list.html"
