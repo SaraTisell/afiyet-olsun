5176c9b5d59119933b40344930f53c8efea7344f
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-16 13:53:35 +0000

    Update booking form with labels

--- booking/forms.py
+++ booking/forms.py
@@ -4,9 +4,9 @@ from datetime import datetime
 
 
 class BookingForm(forms.ModelForm):
-    reservation_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'max': datetime.now().date()}))
+    reservation_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'max': datetime.now().date()}), label='Date')
 
-    reservation_time = forms.ChoiceField(choices=RESERVATION_TIME, widget=forms.RadioSelect())
+    reservation_time = forms.ChoiceField(choices=RESERVATION_TIME, widget=forms.RadioSelect(), label='Time')
 
     class Meta:
         model = Reservation
@@ -20,8 +20,6 @@ class BookingForm(forms.ModelForm):
 
         labels = {
             'guest_name': 'Name',
-            'reservation_date': 'Date',
-            'reservation_time': 'Time',
             'company_size': 'Number of Guests',
             'additional_info': 'Additional Information'
         }
