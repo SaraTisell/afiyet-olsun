8ccbdf01928f6aba9accd298c1d27256c4c32fde
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-16 09:10:27 +0000

    Update css to push footer to the bottom

--- afiyet_olsun/settings.py
+++ afiyet_olsun/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-saratisell-afiyetolsun-4qf96iqq46g.ws-eu108.gitpod.io', '.herokuapp.com']
+ALLOWED_HOSTS = ['8000-saratisell-afiyetolsun-25n5coow1ps.ws-eu108.gitpod.io', '.herokuapp.com']
 
 
 # Application definition
@@ -105,7 +105,7 @@ DATABASES = {
 }
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://*8000-saratisell-afiyetolsun-4qf96iqq46g.ws-eu108.gitpod.io/",
+    "https://*8000-saratisell-afiyetolsun-25n5coow1ps.ws-eu108.gitpod.io/",
     "https://*.herokuapp.com"
 ]
 
