a6a9c3739df473339be4a0585e4bb869fa0fd7ae
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-16 13:38:07 +0000

    Update booking form

--- booking/forms.py
+++ booking/forms.py
@@ -1,7 +1,27 @@
 from django import forms
-from .models import Reservation
+from .models import Reservation, RESERVATION_TIME
+from datetime import datetime
 
 
 class BookingForm(forms.ModelForm):
+    reservation_date = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'max': datetime.now().date()}))
+
+    reservation_time = forms.ChoiceField(choices=RESERVATION_TIME, widget=forms.RadioSelect())
+
     class Meta:
         model = Reservation
+        fields = [
+            'guest_name',
+            'reservation_date',
+            'reservation_time',
+            'company_size',
+            'additional_info'
+        ]
+
+        labels = {
+            'guest_name': 'Name',
+            'reservation_date': 'Date',
+            'reservation_time': 'Time',
+            'company_size': 'Number of Guests',
+            'additional_info': 'Additional Information'
+        }
--- booking/views.py
+++ booking/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import Reservation
+from .forms import BookingForm
 
 
 # Create your views here.
 def booking(request):
-    return render(request, 'booking/booking.html')
+    form = BookingForm()
+    return render(request, 'booking/booking.html', {'booking_form': form})
