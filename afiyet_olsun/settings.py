e5339485f96d842511f78d599c8759461a94217d
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-18 12:15:39 +0000

    Update debug

--- afiyet_olsun/settings.py
+++ afiyet_olsun/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 ALLOWED_HOSTS = ['8000-saratisell-afiyetolsun-3q4pv8kjjtk.ws-eu108.gitpod.io', '.herokuapp.com']
