3554d2cacd36692c35e17e9102599a94391f51e9
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-19 19:42:48 +0000

    Update model & add func to handle post request

--- contact/models.py
+++ contact/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 
 class ContactRequest(models.Model):
+    """ Model for Contact for """
     name = models.CharField(max_length=50)
     email = models.EmailField()
     title = models.CharField(max_length=50)
     message = models.TextField()
     read = models.BooleanField(default=False)
 
-    def _str__(self):
-        return f'Message from {self.name}'
+    def __str__(self):
+        read_icon = "✅" if self.read else "✉"
+        return f'Message from {self.name} | {self.title} | {read_icon}'
--- contact/views.py
+++ contact/views.py
@@ -3,6 +3,13 @@ from .forms import ContactUsForm
 
 
 def contact_us(request):
+
+    if request.method == "POST":
+        contactus_form = ContactUsForm(data=request.POST)
+        if contactus_form.is_valid():
+            contactus_form.save()
+
+
     contactus_form = ContactUsForm()
 
     return render(
