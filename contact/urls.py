a1bcec5c38263d4ed20f7eae103c1b3a695ed69b
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-21 10:08:09 +0000

    Add contact_succes to display after contact form submit

--- contact/urls.py
+++ contact/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.contact_us, name='contact'),
+    path('contact_success/', views.form_success, name='contact_success')
+
 ]
--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ContactUsForm
 
 
+# Return Thank you page 
+def form_success(request):
+    return render(request, 'contact/contact_success.html')
+
+
 def contact_us(request):
+    """
+    View to contact form for users to send a request
+    Account is not mandatory to send a contact request
+    After submission, user is sent to a 'Thank You page'
+    """
 
     if request.method == "POST":
         contactus_form = ContactUsForm(data=request.POST)
         if contactus_form.is_valid():
             contactus_form.save()
+            return redirect('contact_success')
+            
 
 
     contactus_form = ContactUsForm()
