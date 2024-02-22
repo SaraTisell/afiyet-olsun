b1860056e15a5706c6ebe50e52214299fad281ff
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-22 16:06:45 +0000

    Add docstrings

--- contact/views.py
+++ contact/views.py
@@ -1,12 +1,15 @@
 from django.views.generic import CreateView, TemplateView
 from django.urls import reverse_lazy
-#from django.shortcuts import render, redirect
-#from django.contrib import messages
 from .models import ContactRequest
 from .forms import ContactUsForm
 
 
 class ContactUsView(CreateView):
+    """
+    View to contact form for user to send a request
+    Account is not mandatory to send a contact request
+    After submission is the user sent to a "Thank You" page
+    """
     model = ContactRequest
     form_class = ContactUsForm
     template_name = 'contact/contact.html'
@@ -14,4 +17,7 @@ class ContactUsView(CreateView):
 
 
 class ContactSuccessView(TemplateView):
+    """
+    Renders "Thank you" page after contact form submission
+    """
     template_name = 'contact/contact_success.html'
