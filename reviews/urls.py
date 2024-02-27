4b1d6d7feb02aeba7fc77c081d0a66eeecdd20a9
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-27 17:28:00 +0000

    Add url for managereviewsview

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import LeaveReviewView, ViewReviews, UpdateReview, DeleteReview
+from .views import LeaveReviewView, ViewReviews, UpdateReview, DeleteReview, ManageReviewsView
 
 urlpatterns = [
     
@@ -7,5 +7,6 @@ urlpatterns = [
     path('leave_review', LeaveReviewView.as_view(), name='leave_review'),
     path('review/<int:pk>/update', UpdateReview.as_view(), name='update_review'),
     path('review/<int:pk>/delete', DeleteReview.as_view(), name='delete_review'),
+    path('manage_reviews/', ManageReviewsView.as_view(), name='manage_reviews' )
     
 ]
