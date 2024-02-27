d30a9618fa761dc04d8a8f0b8eeb9b24aac8a7e3
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-27 10:09:49 +0000

    Create form to reviews, update views & add templates for CRUD

--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,49 @@
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib import messages
 from django.shortcuts import render
-from django.views import generic
 from .models import Review
 
-# Create your views here.
-class PostList(generic.ListView):
-    queryset = Review.objects.filter(status=1, approved=True).order_by('created_at')
-#    template_name = "review_list.html"
+class LeaveReviewView(CreateView):
+    """
+    View to render form so
+    users can leave reviews
+    """
+    template_name = 'reviews/all_reviews.html'
+    form_class = ReviewForm
+    success_url = "/reviews/all_reviews/"
+
+    def form_valid(self, form):
+
+        review.save()
+        message = f"Your review have ben submitted and waiting for approval"
+        message.success(self.request, message)
+        return super().form_valid(form)
+
+class ViewReviews(ListView):
+    """
+    View to display all exisiting reviews
+    """
+    model = Review
+    template_name = 'reviews/all_reviews.html'
+
+    def get_queryset(self):
+        return Review.object.filter(status=1)
+
+class UpdateReview(UpdateView):
+    """
+    Function to update an exisiting review
+    """
+    model = Review 
+    form_class = ReviewForm
+    template_name = 'reviews/all_reviews.html'
+    success_url = reverse_lazy('reviews')
+
+
+class DeleteReview(DeleteView):
+    """
+    Function to delete an existing review
+    """
+    model = Review
+    template_name = 'reviews/delete_review_confirm.html'
+    success_url = reverse_lazy('all_reviews')
+    success_message = "The review was successfully deleted!"
