9d71b4b8302a6a627f9339390f306381bc0ea11c
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-29 20:04:29 +0000

    Fix errors from py validator

--- reviews/models.py
+++ reviews/models.py
@@ -8,11 +8,13 @@ class Review(models.Model):
     Model for reviews
     """
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    rating = models.IntegerField(default=1,
+    rating = models.IntegerField(
+        default=1,
         validators=[
             MaxValueValidator(5),
             MinValueValidator(1)
-        ])
+         ]
+    )
     content = models.TextField(max_length=150)
     created_at = models.DateTimeField(auto_now_add=True)
     last_updated = models.DateTimeField(auto_now=True)
@@ -22,4 +24,6 @@ class Review(models.Model):
         ordering = ['created_at']
 
     def __str__(self):
-        return f'{self.author} - {self.created_at.strftime("%B %d, %Y")} | {self.content}'
+        return f'{self.author}-'
+        f'{self.created_at.strftime("%B %d, %Y")} |'
+        f'{self.content}'
