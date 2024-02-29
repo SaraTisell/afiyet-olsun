5da74f2bc69edb151c50332637302afa828e4ad9
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-29 20:28:06 +0000

    Fix errors from py validator

--- booking/views.py
+++ booking/views.py
@@ -1,5 +1,5 @@
 from django.contrib.messages.views import SuccessMessageMixin
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin 
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404, reverse
@@ -17,7 +17,7 @@ class BookingFormView(CreateView):
     template_name = 'booking/booking.html'
     form_class = BookingForm
     success_url = "/booking/reservations/"
-    
+
     def form_valid(self, form):
 
         reservation = form.save(commit=False)
@@ -27,20 +27,24 @@ class BookingFormView(CreateView):
         reservation_date = form.cleaned_data['reservation_date']
         reservation_time = form.cleaned_data['reservation_time']
 
-        # Count how many existing reservations there is on requested date and time
+        # Count how many existing reservations there is
+        # on requested date and time
         existing_table_reservation = Reservation.objects.filter(
             reservation_date=reservation_date,
             reservation_time=reservation_time).count()
 
-        # Checks that there is not 10 existing reservations on the requested date and time
+        # Checks that there is not 10 existing reservations
+        # on the requested date and time
         if existing_table_reservation == 10:
-            message = f"Tables are fully booked for {reservation_date} at {reservation_time}, Please try another date and time"
+            message = f"Tables are fully booked for {reservation_date}"
+            f"at {reservation_time}, Please try another date and time"
             messages.error(self.request, message)
             return super().form_invalid(form)
 
         else:
             reservation.save()
-            message = f"Your reservation for {reservation_date} at {reservation_time} is confirmed!"
+            message = f"Your reservation for {reservation_date}"
+            f"at {reservation_time} is confirmed!"
             messages.success(self.request, message)
             return super().form_valid(form)
 
@@ -51,7 +55,7 @@ class ViewReservations(LoginRequiredMixin, ListView):
     Own reservations for user
     All reservations for staff
     """
-    model = Reservation 
+    model = Reservation
     template_name = 'booking/reservations.html'
 
     def get_queryset(self):
@@ -60,8 +64,6 @@ class ViewReservations(LoginRequiredMixin, ListView):
             return Reservation.objects.all()
         else:
             return Reservation.objects.filter(guest=self.request.user)
-
-  
 
 
 class UpdateReservation(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
@@ -84,15 +86,20 @@ class UpdateReservation(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
         reservation_date = form.cleaned_data['reservation_date']
         reservation_time = form.cleaned_data['reservation_time']
 
-        # Count how many existing reservations there is on requested date and time
+        # Count how many existing reservations there is
+        # on requested date and time
         # Excluding the reservations that is under editing
         existing_table_reservation = Reservation.objects.filter(
             reservation_date=reservation_date,
-            reservation_time=reservation_time).exclude(reservation_id=reservation.reservation_id).count()
+            reservation_time=reservation_time).exclude(
+                reservation_id=reservation.reservation_id).count()
 
-        # Checks that there is not 10 existing reservations on the requested date and time
+        # Checks that there is not 10 existing reservations
+        # on the requested date and time
         if existing_table_reservation == 10:
-            message = f"Sorry the requested {reservation_date} and {reservation_time} is not available. Please try another date and time"
+            message = f"Sorry the requested {reservation_date} and"
+            f"{reservation_time} is not available."
+            "Please try another date and time"
             messages.error(self.request, message)
             return super().form_invalid(form)
 
@@ -105,8 +112,7 @@ class UpdateReservation(UserPassesTestMixin, SuccessMessageMixin, UpdateView):
         if self.request.user.is_staff:
             return True
         else:
-            return self.request.user == self.get_object().guest    
-
+            return self.request.user == self.get_object().guest
 
 
 class DeleteReservation(UserPassesTestMixin, SuccessMessageMixin, DeleteView):
