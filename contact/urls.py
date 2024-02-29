0fc69c6f020651dc1922d7602bc8cd370559a62f
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-29 20:17:44 +0000

    Fix errors from py validator

--- contact/urls.py
+++ contact/urls.py
@@ -1,11 +1,18 @@
 from django.urls import path
-from .views import ContactUsView, ContactSuccessView, ContactInboxView, ContactDetailsView, ContactInboxStaff
+from .views import (
+    ContactUsView,
+    ContactSuccessView,
+    ContactInboxView,
+    ContactDetailsView,
+    ContactInboxStaff
+)
 
 
 urlpatterns = [
     path('', ContactUsView.as_view(), name='contact'),
-    path('contact_success/', ContactSuccessView.as_view(), name='contact_success'),
+    path(
+        'contact_success/',
+        ContactSuccessView.as_view(), name='contact_success'),
     path('inbox/', ContactInboxView.as_view(), name='inbox'),
     path('inbox/<int:pk>/', ContactDetailsView.as_view(), name='inbox_details')
-
 ]
