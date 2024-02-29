746ac04c7f32a23e475fc51d9b1308d79611f5bd
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-29 20:29:54 +0000

    Fix errors from py validator

--- booking/urls.py
+++ booking/urls.py
@@ -1,11 +1,19 @@
 from django.urls import path
-from .views import BookingFormView, ViewReservations, UpdateReservation, DeleteReservation
+from .views import (
+    BookingFormView,
+    ViewReservations,
+    UpdateReservation,
+    DeleteReservation
+)
 
 
 urlpatterns = [
     path('', BookingFormView.as_view(), name='booking'),
     path('reservations/', ViewReservations.as_view(), name='reservations'),
-    path('reservation/<str:pk>/update', UpdateReservation.as_view(), name='reservation_update'),
-    path('reservation/<str:pk>/delete', DeleteReservation.as_view(), name= 'reservation_delete'),
-
+    path(
+        'reservation/<str:pk>/update',
+        UpdateReservation.as_view(), name='reservation_update'),
+    path(
+        'reservation/<str:pk>/delete',
+        DeleteReservation.as_view(), name='reservation_delete'),
 ]
