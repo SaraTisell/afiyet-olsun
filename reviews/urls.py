106f3ea5241cbc0bf8ff64a465b0072ebcb0941e
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-29 19:54:34 +0000

    Fix errors from py validation

--- reviews/urls.py
+++ reviews/urls.py
@@ -1,13 +1,24 @@
 from django.urls import path
-from .views import LeaveReviewView, ViewReviews, UpdateReview, DeleteReview, ManageReviewsView, DisplayUserReviews
+from .views import (
+    LeaveReviewView,
+    ViewReviews,
+    UpdateReview,
+    DeleteReview,
+    ManageReviewsView,
+    DisplayUserReviews
+)
 
 urlpatterns = [
-    
     path('', ViewReviews.as_view(), name='reviews'),
     path('leave_review', LeaveReviewView.as_view(), name='leave_review'),
-    path('review/<int:pk>/update', UpdateReview.as_view(), name='update_review'),
-    path('review/<int:pk>/delete', DeleteReview.as_view(), name='delete_review'),
-    path('manage_reviews/', ManageReviewsView.as_view(), name='manage_reviews' ),
+    path(
+        'review/<int:pk>/update',
+        UpdateReview.as_view(), name='update_review'),
+    path(
+        'review/<int:pk>/delete',
+        DeleteReview.as_view(), name='delete_review'),
+    path(
+        'manage_reviews/',
+        ManageReviewsView.as_view(), name='manage_reviews'),
     path('user_reviews/', DisplayUserReviews.as_view(), name='user_reviews')
-    
 ]
