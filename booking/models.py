69c9d4d85816a856b9c320d75e25cf92149fc4f9
Sara Tisell <Sara.Tisell@hotmail.com>
2024-02-29 20:32:11 +0000

    Fix errors from py validator

--- booking/models.py
+++ booking/models.py
@@ -3,15 +3,14 @@ from django.contrib.auth.models import User
 from shortuuid.django_fields import ShortUUIDField
 
 
-
 # Avaliable times for a reservation
-RESERVATION_TIME= (
+RESERVATION_TIME = (
     ("12pm", "12:00pm - 1.25pm"),
     ("2pm", "2:00pm - 3:45pm"),
     ("4pm", "4:00pm - 5:45pm"),
     ("6pm", "6:00pm - 7:45pm"),
     ("8pm", "8:00pm - 9:45pm"),
-    
+
 )
 
 
@@ -29,21 +28,24 @@ COMPANY_SIZE = (
 )
 
 
-
 class Reservation(models.Model):
     """ Model to create reservation """
     reservation_id = ShortUUIDField(primary_key=True, length=6, max_length=6)
-    guest = models.ForeignKey(User, on_delete=models.CASCADE, related_name="booking_guest")
+    guest = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="booking_guest")
     guest_name = models.CharField(max_length=50)
     email = models.EmailField()
     reservation_date = models.DateField()
-    reservation_time = models.CharField(max_length=10, choices=RESERVATION_TIME)
+    reservation_time = models.CharField(
+            max_length=10, choices=RESERVATION_TIME)
     company_size = models.CharField(default=1, choices=COMPANY_SIZE)
     additional_info = models.TextField(max_length=200, blank=True, null=True)
-    
+
     class Meta:
         """ Ordering reservations based on date and time """
         ordering = ['reservation_date', 'reservation_time']
 
     def __str__(self):
-        return f'{self.reservation_id} - {self.guest_name} - {self.reservation_date} - {self.reservation_time}'
+        return f'{self.reservation_id} -'
+        f'{self.guest_name} - {self.reservation_date} -'
+        f'{self.reservation_time}'
